ddc4dde670fbfcccfadd6e96b2f93057a27b445b
shyamsrinivasan <shyamsweth@gmail.com>
2024-08-18 16:32:44 +0530

    working fastapi docs

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
-# import logging
+import logging
 
 # from core.config import CoreConfig
 
-# from endpoints.user import router as user_router
+from endpoints.user import router as user_router
 
 
-# logging.basicConfig(filename='app.log', level=logging.INFO)
+logging.basicConfig(filename='daisyAPI.log', level=logging.INFO)
 
 app = FastAPI()
 
-# origins = [
-#     "http://locahost",
-#     "http://locahost:8080", 
-# ]
+origins = [
+    "http://locahost",
+    "http://locahost:8080", 
+]
 
-# app.include_router(user_router, prefix='/user')
+app.include_router(user_router, prefix='/user')
 
-# app.add_middleware(CORSMiddleware, 
-#                    allowed_origins=origins,
-#                    allow_credentials=True,
-#                    allow_methods=["*"],
-#                    allow_headers=["*"],
-#                    )
+app.add_middleware(CORSMiddleware, 
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
 
 
 
