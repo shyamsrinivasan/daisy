41618619df94e86e66e5405f5b2f4fc9a3a1c10d
shyamsrinivasan <shyamsweth@gmail.com>
2024-10-13 12:05:58 +0530

    user add w/o authnetication

--- endpoints/user/__init__.py
+++ endpoints/user/__init__.py
@@ -2,16 +2,18 @@ from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from fastapi.params import Depends
 from fastapi.encoders import jsonable_encoder
+from starlette.status import HTTP_200_OK
 from sqlmodel import Session
 
 from core.auth import AuthHandler
 from schemas.core import ResponseModel
 
 from core.database import get_db
+from core.config import CoreConfig
 
-from schemas.user import UserLogin
+from schemas.user import UserLogin, UserAdd
 
-from repos.user import user_from_username
+from repos.user import user_from_username, add_new_user
 
 
 router = APIRouter()
@@ -32,4 +34,17 @@ async def login(user: UserLogin, db: Session = Depends(get_db)):
     token = auth_handler.encode_token(user_obj.UserName, user_obj.UserId)
     response = ResponseModel(data=jsonable_encoder({'token': token, 'user_details': []}))
     return JSONResponse(jsonable_encoder(response), status_code=status.HTTP_200_OK)    
-    
+
+
+@router.post('/add', tags=['user', 'add'], description='Add new user', response_model=ResponseModel)
+async def add_user(user_add: UserAdd, db: Session = Depends(get_db)):      
+    new_user_obj = await add_new_user(user_add, db)
+    if new_user_obj == 1:
+        response = ResponseModel(status=400, message='Username already exists')
+        return JSONResponse(jsonable_encoder(response), status_code=HTTP_200_OK)
+    if new_user_obj == 2:
+        response = ResponseModel(status=400, message='User email already exists')
+        return JSONResponse(jsonable_encoder(response), status_code=HTTP_200_OK)
+    response = ResponseModel(data=jsonable_encoder(new_user_obj))
+    return JSONResponse(jsonable_encoder(response), status_code=status.HTTP_200_OK)   
+ 
--- schemas/user.py
+++ schemas/user.py
@@ -1,7 +1,16 @@
 from sqlmodel import SQLModel
+from typing import Optional
 
 
 class UserLogin(SQLModel):
     domain: str
     username: str
     password: str
+
+
+class UserAdd(SQLModel):
+    UserName: str    
+    FirstName: str
+    LastName: Optional[str] = None
+    Email: str
+    
